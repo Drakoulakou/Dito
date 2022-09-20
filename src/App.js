e017e5d83b9f264dbad56b2dd697e139a6bc2c67
Eleftheria <drakelef@gmail.com>
2022-09-20 19:32:36 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Layout from './components/Layout';
 import HomePage from './pages/Home';
+import CartPage from './pages/Cart';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
 
   return (
     <Layout>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path='/cart' element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
     </Layout>
   )
 }
---
+++ src/pages/Cart/index.js
@@ -0,0 +1,11 @@
+import './styles.css'
+
+function CartPage(){
+    return(
+        <main>
+            <h1>My cart</h1>
+        </main>
+    )
+}
+
+export default CartPage
